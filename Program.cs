d26c076841fef630f2a2c8825635abbfe222390e
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2023-10-06 09:45:57 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        //        static void Main(string[] args)
+        //        {
+        //            string value1 = TakeValue();
+        //            string value2 = TakeValue();
+
+        //            if (IntOlubOlmadiqiniYoxla(value1) && IntOlubOlmadiqiniYoxla(value2))
+        //            {
+        //                int value1Int = int.Parse(value1);
+        //                int value2Int = int.Parse(value2);
+        //                int sum = summ(value1Int, value2Int);
+        //                Console.WriteLine($"sum: {sum}");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Her iki deyer integer deyil.");
+        //            }
+        //        }
+        //        static string TakeValue()
+        //        {
+        //            Console.Write("Bir deyer daxil edin: ");
+        //            return Console.ReadLine();
+        //        }
+
+        //        static bool IntOlubOlmadiqiniYoxla(string value)
+        //        {
+        //            int count;
+        //            return int.TryParse(value, out count);
+        //        }
+
+        //        static int summ(int a, int b)
+        //        {
+        //            return a + b;
+        //        }
+
+
+
+        //static void Main()
+        //{
+        //    int[] intArray = { 1, 2, 3, 4, 5 };
+        //    int newvalue = 6;
+
+        //    Console.WriteLine("before array:");
+        //    foreach (int element in intArray)
+        //    {
+        //        Console.WriteLine(element + " ");
+        //    }
+
+        //    Add Value(ref int Array, newvalue);
+            
+        //    Console.WriteLine("new array:");
+        //    foreach (int element in intArray)
+        //    {
+        //        Console.WriteLine(element + " ");
+        //    }
+        //}
+        //static void Add Value(ref int[] array, int value)
+        //{
+        //    Array.Resize(ref array, array.Length + 1);
+        //    array[array.Length - 1] = value;
+        //}
+
+    }
+}
